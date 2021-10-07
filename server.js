abdd5c7966de5efef97eed2eabe331c41d7d0a4f
Khanh Pham <pdkhanh93@gmail.com>
2021-10-07 20:18:33 +0700

    init

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+app.get("/", (req, res) => {
+    res.json({ message: "Version 1" });
+});
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}.`);
+});
