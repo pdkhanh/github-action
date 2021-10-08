40431d830eae3dd860d154053cf1d9a341e90092
Khanh Pham <pdkhanh93@gmail.com>
2021-10-08 14:38:40 +0700

    24

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-    res.json({ message: "Version 23" });
+    res.json({ message: "Version 24" });
 });
 
 const PORT = process.env.PORT || 8080;
