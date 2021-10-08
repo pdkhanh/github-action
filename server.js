540baac3d77b83046cbf02c747e7d053e6fff400
Khanh Pham <pdkhanh93@gmail.com>
2021-10-08 11:33:47 +0700

    Deploy Production

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-    res.json({ message: "Version 17" });
+    res.json({ message: "Version 18" });
 });
 
 const PORT = process.env.PORT || 8080;
